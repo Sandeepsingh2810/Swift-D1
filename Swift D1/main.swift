2756e3bc23d309fa447b6d1e708520b82d6af76b
sandeepsingh2810 <sandeepjangra0100@gmail.com>
2019-10-07 20:20:42 -0400

    Initial Commit

---
+++ Swift D1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift D1
+//
+//  Created by MacStudent on 2019-10-07.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
